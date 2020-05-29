faf6087c99c434237a80e8d32f99604dc865c227
Skayo <jonas@skayo.dev>
2020-05-29 13:40:27 +0200

    Switched to SVG for logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,11 @@
 import UIkit from 'uikit';
 import Icons from 'uikit/dist/js/uikit-icons';
+import logo from '../img/logo.png';
 
 // loads the Icon plugin
 UIkit.use(Icons);
+
+document.getElementById('logo').onerror = function() {
+	this.src = logo;
+	this.onerror = null;
+};
