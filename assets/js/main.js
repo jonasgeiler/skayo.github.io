aebc480dd604dbf6f2e65953a1c23d09048d9a8b
Skayo <jonas@skayo.dev>
2020-05-29 12:39:30 +0200

    Switched to Parcel.js Completely reworked build process

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
-const UIkit = require('uikit');
-const Icons = require('uikit/dist/js/uikit-icons');
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
 
 // loads the Icon plugin
 UIkit.use(Icons);
